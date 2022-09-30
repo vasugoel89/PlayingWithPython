dcc8b1ecd11f75e1e2b376f7abb9eb900379c9e7
Vasu Goel <vasugoel89@gmail.com>
2022-09-30 23:18:26 +0530

    play python

---
+++ WBot/bot.py
@@ -0,0 +1,8 @@
+from calendar import day_abbr
+from ssl import OPENSSL_VERSION_NUMBER
+import pyautogui as auto
+import time
+while True:
+    auto.write("Python WBot")
+    auto.press('enter')
+    time.sleep(1)
